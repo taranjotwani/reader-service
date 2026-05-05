24a34f7dc801f4713064d6897cc4a9b4de77ac64
Taran Jotwani <taran.obdev@gmail.com>
2026-05-05 16:41:05 -0700

    initial commit

---
+++ src/main/java/com/taran/ecs/HelloController.java
@@ -0,0 +1,18 @@
+package com.taran.ecs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public Map<String, String> sayHello() {
+		Map<String, String> response = new HashMap<>();
+		response.put("message", "Hello from Deployment!");
+		return response;
+	}
+}
